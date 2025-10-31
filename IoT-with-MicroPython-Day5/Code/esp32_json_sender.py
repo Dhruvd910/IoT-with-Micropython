dedff520654b27cab07d3e4b046970e08c31000e
Dhruvd910 <64086720+Dhruvd910@users.noreply.github.com>
2025-10-31 09:59:46 +0530

    Refactor esp32_json_sender to esp32_json_publisher

--- IoT-with-MicroPython-Day5/Code/esp32_json_sender.py
+++ IoT-with-MicroPython-Day5/Code/esp32_json_sender.py
@@ -1,17 +1,38 @@
-# esp32_json_sender.py
-import ujson
-import time
+# esp32_json_publisher.py
+import network, time, random
+from umqtt.simple import MQTTClient
+import json
 
-# Simulated sensor data
-temperature = 25.3
-humidity = 60
+WIFI_SSID = "ALICE"
+WIFI_PASSWORD = "9811255833"
+BROKER = "192.168.137.134" #raspberry ip 
+TOPIC = b"iot/jsondata" #your topic name
 
-while True:
-    payload = {
-        "device": "ESP32",
-        "temperature": temperature,
-        "humidity": humidity,
-        "timestamp": time.time()
-    }
-    print(ujson.dumps(payload))  # Send as JSON string
-    time.sleep(5)
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+    while not wlan.isconnected():
+        time.sleep(0.5)
+    print("✅ WiFi connected:", wlan.ifconfig())
+
+def main():
+    connect_wifi()
+    client = MQTTClient("ESP32_JSON", BROKER)
+    client.connect()
+    print("📡 Connected to MQTT Broker")
+
+    while True:
+        data = {
+            "device_id": "ESP32_1",
+            "temperature": round(random.uniform(25.0, 30.0), 2),
+            "humidity": round(random.uniform(40.0, 60.0), 2),
+            "timestamp": time.time()
+        }
+        json_data = json.dumps(data)
+        client.publish(TOPIC, json_data)
+        print("📤 Sent:", json_data)
+        time.sleep(5)
+
+main()
+
